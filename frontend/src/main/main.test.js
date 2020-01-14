38f1e6b1a592d1b815151b523541f83af6bfcb35
Ryaan Ahmed <rahmed@mit.edu>
2020-01-14 10:36:48 -0500

    ci settings, lint

--- frontend/src/main/main.test.js
+++ frontend/src/main/main.test.js
@@ -4,6 +4,6 @@ import MainView from './main';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<MainView />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
