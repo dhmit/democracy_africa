02a869453f99e11d77bd739e55d386c2f80fe4fa
Amy <leiamy12@gmail.com>
2020-01-21 17:01:44 -0500

    added notes based on what evan wants

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,3 +3,10 @@ Models for the democracy_africa app.
 """
 # from django.db import models
 # TODO: implement me!
+
+# budget allocation, material quality of life
+# sometimes negative effects
+# WHAT MATTERS: electricity, water, access to sanitation, co-ethnicity **,  type of housing,
+# urban/rural***,
+
+# datasets: afrobarometer
