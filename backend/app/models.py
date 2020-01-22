0b00f156deaa177b70895c1ce432e10e589c9dfb
Amy <leiamy12@gmail.com>
2020-01-22 15:50:34 -0500

    documented api format and corresponding effects on budget

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,27 +5,25 @@ Models for the democracy_africa app.
 # TODO: implement me!
 
 """
-WHAT MATTERS: 
-- electricity
-- water
-- access to sanitation
-- co-ethnicity **
-- type of housing
-- urban/rural***
 
-budget allocation, material quality of life
-data set: afrobarometer
+For the budget allocation, if three needs are met, then the citizen is in favor.
+What counts as meets the needs? When a percentage of the budget is allocated to it. 
+
+{
+    id: String,
+    name: String,
+    traits: {
+        lives_in_rural_area: boolean,                   // affects desire for infrastructure 
+        has_access_to_electricity: boolean,             // affects desire for furthering electricity 
+        has_access_to_water: boolean,                   // affects desire for furthering water
+        has_access_to_sanitation: boolean,              // affects desire for furthering sanitation
+        finished_high_school: boolean,                  // together with college, affects desire
+        finished_college: boolean,                      // for furthering education 
+    }
+    will_support: boolean, 
+}
+
 """
-
-
-# from django.db import models
-#
-# class Citizen(models.model):
-#     name = models.CharField()
-#     gender = models.CharField()
-#
-#     def __str__(self):
-#         return f'Citizen: {self.name}'
 
 
 # TODO: do a simple Python class with properties. Once we have real data, convert to Django models.
@@ -48,34 +46,34 @@ class Citizen:
         self.housing_tenure = tenure
 
     # Don't think we actually need this, but I already wrote it and didn't want to delete it yet
-    # def __str__(self):
-    #     returnString = self.id + "\n"
-    #     returnString += "Age: " + self.age + "\n"
-    #     returnString += "In the " + self.poverty_level + " income bracket\n"
-    #     returnString += "Lives in a(n) " + self.housing_type + " in a(n) " + self.house_location + \
-    #                     "\n"
-    #
-    #     if self.water_access:
-    #         returnString += "Has access to clean/fresh water"
-    #     else:
-    #         returnString += "Does not have access to clean/fresh water"
-    #     returnString += "\n"
-    #
-    #     if self.electricity_access:
-    #         returnString += "Has access to electricity"
-    #     else:
-    #         returnString += "Does not have access to electricity"
-    #     returnString += "\n"
-    #
-    #     if self.sanitation_access:
-    #         returnString += "Has access to sanitation"
-    #     else:
-    #         returnString += "Does not have access to sanitation"
-    #     returnString += "\n"
-    #
-    #     returnString += "Identifies ethnically as " + self.coethnicity + " and as a " + self.sex
-    #
-    #     return returnString
+    def __str__(self):
+        returnString = self.id + "\n"
+        returnString += "Age: " + self.age + "\n"
+        returnString += "In the " + self.poverty_level + " income bracket\n"
+        returnString += "Lives in a(n) " + self.housing_type + " in a(n) " + self.house_location + \
+                        "\n"
+
+        if self.water_access:
+            returnString += "Has access to clean/fresh water"
+        else:
+            returnString += "Does not have access to clean/fresh water"
+        returnString += "\n"
+
+        if self.electricity_access:
+            returnString += "Has access to electricity"
+        else:
+            returnString += "Does not have access to electricity"
+        returnString += "\n"
+
+        if self.sanitation_access:
+            returnString += "Has access to sanitation"
+        else:
+            returnString += "Does not have access to sanitation"
+        returnString += "\n"
+
+        returnString += "Identifies ethnically as " + self.coethnicity + " and as a " + self.sex
+
+        return returnString
 
 
 cit1 = Citizen("Jordan", "apartment", "urban", True, True, True, "Caucasion", "Male")
@@ -89,7 +87,11 @@ class StatisticalDistributions:
         self.sex = {"male": .482, "female": .518}
         self.education = {"none": .086, "some primary": .123, "completed primary": .046,
                           "some secondary": .339, "grade 12": .285, "higher": .121}
-        self.water_access = {"none": .088, "outside the yard": .179, "inside dwelling/yard": .734}
+        self.water_access = {
+            "none": .088,
+            "outside the yard": .179,
+            "inside dwelling/yard": .734
+        }
         self.housing_tenure = {"own/paid off": .413, "own/not paid off": .118, "rent": .25,
                                "rent-free": .186}
         self.refuse_disposal = {"weekly by local authority": .621, "local authority": .015,
