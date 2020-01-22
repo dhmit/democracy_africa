4a503feae7b946211de5aa102c786ad923964221
Amy <leiamy12@gmail.com>
2020-01-22 11:39:34 -0500

    made basice ctiizen class

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,9 +4,34 @@ Models for the democracy_africa app.
 # from django.db import models
 # TODO: implement me!
 
-# budget allocation, material quality of life
-# sometimes negative effects
-# WHAT MATTERS: electricity, water, access to sanitation, co-ethnicity **,  type of housing,
-# urban/rural***,
+"""
+WHAT MATTERS: 
+- electricity
+- water
+- access to sanitation
+- co-ethnicity **
+- type of housing
+- urban/rural***
 
-# datasets: afrobarometer
+budget allocation, material quality of life
+data set: afrobarometer
+"""
+
+# from django.db import models
+#
+# class Citizen(models.model):
+#     name = models.CharField()
+#     gender = models.CharField()
+#
+#     def __str__(self):
+#         return f'Citizen: {self.name}'
+
+
+# TODO: do a simple Python class with properties. Once we have real data, convert to Django models.
+class Citizen:
+    def __init___(self, id):
+        self.id = id
+
+
+
+
--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -3,6 +3,13 @@ Serializers take models or other data structures and present them
 in ways that can be transported across the backend/frontend divide, or
 allow the frontend to suggest changes to the backend/database.
 """
-# from rest_framework import serializers
+from rest_framework import serializers
+from .models import (
+    Citizen,
+)
 
 #  TODO: implement me!
+class CitizenSriealizer(serializers.modelSerializer):
+    """
+    Serializes a Citizen
+    """
