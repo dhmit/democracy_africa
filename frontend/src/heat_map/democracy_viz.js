26aab87aad36d6269e32b68502cd6ea7402b8871
Mayowa99 <47461505+Mayowa99@users.noreply.github.com>
2020-01-27 16:41:41 -0500

    centered map and slider on the page, added possible map color schemes

--- frontend/src/heat_map/democracy_viz.js
+++ frontend/src/heat_map/democracy_viz.js
@@ -67,25 +67,16 @@ export class DemocracyViz extends  React.Component {
                 </select>
                 <br/>
                 <div className = 'slidecontainer'>
-                    <input onChange={(e) => this.handleYearChange(e)}
-                        type='range' id = 'year' name = 'year' min = '1981' max = '2018' step = '1'
-                        className= 'slider'>
-                    </input>
+                    <div className={'map'}>
+                        <input onChange={(e) => this.handleYearChange(e)}
+                            type='range' id = 'year' name = 'year' min = '1981' max = '2018'
+                            step = '1' className= 'slider'>
+                        </input>
+                    </div>
                     {this.state.year}
                 </div>
 
                 <br/>
-                {/*Year: &nbsp;*/}
-                {/*<select onChange={(e) => this.handleYearChange(e)}>*/}
-                {/*    {Object.keys(this.state.democracyData[0]["democracy_scores"]).map(*/}
-                {/*        (entry, i) => (*/}
-                {/*            <option key={i}>*/}
-                {/*                {entry}*/}
-                {/*            </option>*/}
-                {/*        )*/}
-                {/*    )}*/}
-                {/*</select>*/}
-                {/*<br/>*/}
                 Currently grey either means:
                 <ul>
                     {/*Remove this later once we fix data*/}
@@ -97,11 +88,14 @@ export class DemocracyViz extends  React.Component {
                     </li>
                 </ul>
                 <br/>
-                <DemocracyMap
-                    democracyData={this.state.democracyData}
-                    scoreType={this.state.scoreType}
-                    year={this.state.year}
-                />
+                <div className={'map'}>
+                    <DemocracyMap
+                        democracyData={this.state.democracyData}
+                        scoreType={this.state.scoreType}
+                        year={this.state.year}
+
+                    />
+                </div>
             </>
         )
     }
@@ -122,7 +116,43 @@ export class DemocracyMap extends React.Component {
         this.getCountryData = this.getCountryData.bind(this);
         this.colorScale = d3.scaleLinear()
             .domain([0, 1])
-            .range(['white', 'maroon'])
+            ///this seems like a nice africa color scheme so maybe try to figure out how to
+            // add it
+
+            //.range(["#ffffe5","#ffffe4","#fffee2","#fffee1","#fffee0","#fffedf","#fffddd",
+            // "#fffddc",
+            //     "#fffddb","#fffdd9","#fffcd8","#fffcd7","#fffcd6","#fffcd4","#fffbd3","#fffbd2",
+            //     "#fffbd0","#fffacf","#ffface","#fffacc","#fff9cb","#fff9ca","#fff9c9","#fff8c7",
+            //     "#fff8c6","#fff8c5","#fff7c3","#fff7c2","#fff7c1","#fff6bf","#fff6be","#fff5bd",
+            //     "#fff5bc","#fff4ba","#fff4b9","#fff4b8","#fff3b6","#fff3b5","#fff2b4","#fff2b2",
+            //     "#fff1b1","#fff1af","#fff0ae","#ffefad","#ffefab","#ffeeaa","#ffeea9","#ffeda7",
+            //     "#feeca6","#feeca4","#feeba3","#feeaa1","#feeaa0","#fee99e","#fee89d","#fee89b",
+            //     "#fee79a","#fee698","#fee697","#fee595","#fee493","#fee392","#fee390","#fee28e",
+            //     "#fee18d","#fee08b","#fedf89","#fedf87","#fede86","#fedd84","#fedc82","#fedb80",
+            //     "#feda7e","#fed97d","#fed87b","#fed779","#fed777","#fed675","#fed573","#fed471",
+            //     "#fed370","#fed26e","#fed16c","#fed06a","#fecf68","#fece66","#fecd64","#fecc63",
+            //     "#fecb61","#fec95f","#fec85d","#fec75b","#fec65a","#fec558","#fec456","#fec355",
+            //     "#fec253","#fec051","#febf50","#febe4e","#febd4d","#febc4b","#feba4a","#feb948",
+            //     "#feb847","#feb746","#feb544","#feb443","#feb341","#feb240","#feb03f","#feaf3e",
+            //     "#feae3c","#feac3b","#fdab3a","#fdaa39","#fda938","#fda737","#fda635","#fda534",
+            //     "#fda333","#fca232","#fca131","#fc9f30","#fc9e2f","#fc9d2e","#fb9b2d","#fb9a2c",
+            //     "#fb992b","#fb972a","#fa962a","#fa9529","#fa9328","#f99227","#f99126","#f89025",
+            //     "#f88e25","#f88d24","#f78c23","#f78a22","#f68921","#f68821","#f58620","#f5851f",
+            //     "#f4841f","#f3831e","#f3811d","#f2801c","#f27f1c","#f17e1b","#f07c1a","#f07b1a",
+            //     "#ef7a19","#ee7918","#ee7718","#ed7617","#ec7517","#eb7416","#eb7215","#ea7115",
+            //     "#e97014","#e86f14","#e86e13","#e76c12","#e66b12","#e56a11","#e46911","#e36810",
+            //     "#e2670f","#e1650f","#e1640e","#e0630e","#df620d","#de610d","#dd600c","#dc5f0c",
+            //     "#db5e0b","#da5d0b","#d95b0a","#d75a0a","#d65909","#d55809","#d45708","#d35608",
+            //     "#d25508","#d15407","#cf5307","#ce5207","#cd5106","#cc5006","#ca4f06","#c94e05",
+            //     "#c84d05","#c74c05","#c54b05","#c44b05","#c24a04","#c14904","#c04804","#be4704",
+            //     "#bd4604","#bb4504","#ba4504","#b84404","#b74304","#b54203","#b44103","#b24103",
+            //     "#b14003","#af3f03","#ae3e03","#ac3e03","#ab3d03","#a93c03","#a83b04","#a63b04",
+            //     "#a43a04","#a33904","#a13904","#a03804","#9e3704","#9c3704","#9b3604","#993604",
+            //     "#983504","#963404","#943404","#933304","#913304","#903204","#8e3104","#8c3104",
+            //     "#8b3005","#893005","#882f05","#862f05","#842e05","#832e05","#812d05","#802d05",
+            //     "#7e2c05","#7c2c05","#7b2b05","#792b05","#782a05","#762a05","#742905","#732905",
+            //     "#712806","#702806","#6e2706","#6c2706","#6b2606","#692606","#682506","#662506"])
+            .range(['red', 'gold'])
     }
 
     /**
