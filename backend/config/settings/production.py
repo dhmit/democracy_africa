e928212e4c1061eddbcdc05c4ff1adbc182c2c64
Ryaan Ahmed <rahmed@mit.edu>
2020-02-03 14:19:43 -0500

    add deploy files

---
+++ backend/config/settings/production.py
@@ -0,0 +1,21 @@
+"""
+
+Production settings for dhmit/democracy_africa
+
+"""
+
+from .base import *  # pylint: disable=unused-wildcard-import, wildcard-import
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  # set in venv activate
+
+ADMINS = ['rahmed@mit.edu']  # Django will email Ryaan on internal server errors
+
+ALLOWED_HOSTS = [
+    'democracy-africa.dhmit.xyz',
+]
+
+
+
