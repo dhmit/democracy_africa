757427e7b081bed75fa2300f4553210713fcb8dd
Ubuntu <ubuntu@ip-172-31-10-23.us-west-1.compute.internal>
2020-02-03 22:57:46 +0000

    deploy process changes

--- frontend/config/paths.js
+++ frontend/config/paths.js
@@ -82,7 +82,7 @@ module.exports = {
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
+  servedPath: 'static/bundles/',
   statsRoot: resolveApp('../'),
 };
 
