86fdf2744e9570ed151239b2cd3c1f17137b5dbd
Ivy Mao <maoivy01@gmail.com>
2020-02-18 16:25:59 -0500

    Created files needed

---
+++ frontend/src/chooseAdventure/chooseAdventure.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+
+// TODO: hardcode data structure here
+
+/**
+ * Component for displaying choose your own adventure skeleton
+ */
+
+export class chooseAdventure extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return ( <div>hello</div> );
+
+    }
+}
