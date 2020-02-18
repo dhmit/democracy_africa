9bf41f3fe4ab365414096379c5f96661d5f31853
Amy <leiamy12@gmail.com>
2020-02-18 17:06:11 -0500

    made maps scalable via prop

--- frontend/src/slaveTradeViz/slaveTradeViz.js
+++ frontend/src/slaveTradeViz/slaveTradeViz.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as PropTypes from 'prop-types';
+
 import {
     project_features_and_create_svg_paths,
 } from "../common";
@@ -17,7 +19,7 @@ class AfricaMap extends React.Component {
         try {
             const res = await fetch('/api/africa_map_geojson/');
             const geo_json = await res.json();
-            const map_data = project_features_and_create_svg_paths(geo_json, 250);
+            const map_data = project_features_and_create_svg_paths(geo_json, this.props.scale);
             this.setState({
                 map_data: map_data,
             });
@@ -34,8 +36,8 @@ class AfricaMap extends React.Component {
         return(
             <>
                 <svg
-                    height="1000"
-                    width="1000"
+                    height={this.props.scale * 1.5}
+                    width={this.props.scale * 1.5}
                 >
                     {this.state.map_data.map((country, i) =>
                         (<MapPath
@@ -52,6 +54,9 @@ class AfricaMap extends React.Component {
         );
     }
 }
+AfricaMap.propTypes = {
+    scale: PropTypes.number,
+};
 
 export class SlaveTradeViz extends React.Component {
     constructor(props) {
@@ -61,8 +66,12 @@ export class SlaveTradeViz extends React.Component {
         return (
             <>
                 <h1>Hello World</h1>
-                <AfricaMap/>
-                <AfricaMap/>
+                <AfricaMap
+                    scale={350}
+                />
+                <AfricaMap
+                    scale={350}
+                />
             </>
         );
     }
