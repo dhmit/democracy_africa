1095282e4f6f27436d767a6d06d8fadddb91c178
Amy <leiamy12@gmail.com>
2020-02-19 16:26:02 -0500

    map fill now determined by external value

--- frontend/src/slaveTradeViz/slaveTradeViz.js
+++ frontend/src/slaveTradeViz/slaveTradeViz.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 import {
     project_features_and_create_svg_paths,
 } from "../common";
 
 import { MapPath } from "../UILibrary/components";
+import * as d3 from "d3";
 
 class AfricaMap extends React.Component {
     constructor(props) {
@@ -32,23 +35,46 @@ class AfricaMap extends React.Component {
         if (!this.state.map_data) {
             return(<div>Loading!</div>);
         }
-
+        const colorScale = d3.scaleLinear()
+            .domain([0, 1])
+            .range(['yellow', 'red']);
         return(
             <>
                 <svg
                     height={this.props.scale * 1.5}
                     width={this.props.scale * 1.5}
                 >
-                    {this.state.map_data.map((country, i) =>
-                        (<MapPath
-                            key={i}
-                            path={country.svg_path}
-                            fill="grey"
-                            stroke="black"
-                            strokeWidth="1"
-                            useColorTransition={true}
-                        />),
-                    )}
+                    {this.state.map_data.map((country, i) => {
+                        const info = (
+                            <Popover id="popover-basic">
+                                <Popover.Title as="h3">
+                                    Africa
+                                </Popover.Title>
+                                <Popover.Content>
+                                    <div>Hello world</div>
+                                </Popover.Content>
+                            </Popover>);
+
+                        const random_num = Math.random();
+
+                        return (
+                            <OverlayTrigger
+                                key={i}
+                                overlay={info}
+                                placement="right"
+                            >
+                                <MapPath
+                                    key={i}
+                                    path={country.svg_path}
+                                    fill={colorScale(random_num)}
+                                    stroke="black"
+                                    strokeWidth="1"
+                                    useColorTransition={true}
+                                />
+                            </OverlayTrigger>
+                        );
+                    })}
+
                 </svg>
             </>
         );
@@ -61,7 +87,17 @@ AfricaMap.propTypes = {
 export class SlaveTradeViz extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            value: "0",
+        };
     }
+
+    handleSliderChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        });
+    }
+
     render() {
         return (
             <>
@@ -71,6 +107,14 @@ export class SlaveTradeViz extends React.Component {
                 />
                 <AfricaMap
                     scale={350}
+                />
+                <input
+                    type="range"
+                    min="0"
+                    max="1"
+                    step="0.01"
+                    value={this.state.value}
+                    onChange={this.handleSliderChange}
                 />
             </>
         );
