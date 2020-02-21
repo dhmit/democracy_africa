8f00c934b4fb9f62dfe5e3acd410f3870d52f686
Dylan Weber <dylanweb@mit.edu>
2020-02-21 15:39:25 -0500

    Switched to using a dictionary rather than direct references

--- frontend/src/chooseAdventure/chooseAdventure.js
+++ frontend/src/chooseAdventure/chooseAdventure.js
@@ -3,6 +3,18 @@ import React from 'react';
 
 // TODO: hardcode data structure here
 
+const START_STAGE = {
+    'text': 'Your school district\'s budget was cut!',
+    'options': [{
+        'text': 'Start a media campaign',
+        'stageName': 'MEDIA_STAGE',
+    },
+    {
+        'text': 'Take direct action',
+        'stageName': 'DIRECT_STAGE',
+    }],
+};
+
 const MEDIA_STAGE = {
     'text': 'You chose to start a media campaign.',
     'options': [{
@@ -26,18 +38,15 @@ const DIRECT_STAGE = {
     }],
 };
 
-const START_STAGE = {
-    'text': 'Your school district\'s budget was cut!',
-    'options': [{
-        'text': 'Start a media campaign',
-        'stage': MEDIA_STAGE,
-    },
-    {
-        'text': 'Take direct action',
-        'stage': DIRECT_STAGE,
-    }],
-};
+const NAME_TO_STAGE = {'START_STAGE' : START_STAGE,
+    'MEDIA_STAGE' : MEDIA_STAGE,
+    'DIRECT_STAGE' : DIRECT_STAGE};
 
+//TODO use this so that we can get rid of the nolint
+// eslint-disable-next-line no-unused-vars
+function getStageFromOption(option) {
+    return NAME_TO_STAGE[option.stageName];
+}
 
 /**
  * Component for displaying choose your own adventure skeleton
@@ -52,8 +61,6 @@ export class chooseAdventureView extends React.Component {
     }
 
     render() {
-
         return ( <div>hello</div> );
-
     }
 }
