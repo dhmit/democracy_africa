9b73685b542594a78a83da005afe63a1cc5ae0c4
Dylan Weber <dylanweb@mit.edu>
2020-03-04 15:37:46 -0500

    Rounded the percentages

--- frontend/src/chooseAdventure/endView.js
+++ frontend/src/chooseAdventure/endView.js
@@ -18,14 +18,16 @@ class EndView extends React.Component {
     render() {
         const choices = this.props.history.map((option, k) => (
             <div key={k}>
-                Choosing to {option.text} has a {option.successFactor * 100}% chance of success.
+                Choosing to {option.text} has
+                a {(option.successFactor * 100).toFixed(2)}% chance of success.
                 <br />
                 {option.successDetail}
             </div>
         ));
         return (
             <div>
-                <div>You had {this.props.successTotal * 100}% chance of succeeding.</div>
+                <div>You had a {(this.props.successTotal * 100).toFixed(2)}%
+                    chance of succeeding.</div>
                 {choices}
                 <button onClick={() => this.resetAdventure()}>Try again</button>
             </div>
