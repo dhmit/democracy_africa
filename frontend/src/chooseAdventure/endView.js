709f5de1305267af9e1c4660f8369bdaa683f688
Dylan Weber <dylanweb@mit.edu>
2020-03-04 15:11:20 -0500

    Success factor are printed as percentages so that they are more human-readable

--- frontend/src/chooseAdventure/endView.js
+++ frontend/src/chooseAdventure/endView.js
@@ -18,14 +18,14 @@ class EndView extends React.Component {
     render() {
         const choices = this.props.history.map((option, k) => (
             <div key={k}>
-                Choosing to {option.text} has a {option.successFactor} chance of success.
+                Choosing to {option.text} has a {option.successFactor * 100}% chance of success.
                 <br />
                 {option.successDetail}
             </div>
         ));
         return (
             <div>
-                <div>You had {this.props.successTotal} chance of succeeding.</div>
+                <div>You had {this.props.successTotal * 100}% chance of succeeding.</div>
                 {choices}
                 <button onClick={() => this.resetAdventure()}>Try again</button>
             </div>
