864f35843e1cd70638b16eef5776a84d82c1e876
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2020-06-12 15:53:42 -0400

    Start of Drag-and-Drop System of managing priotity points

--- frontend/src/campaign/campaignView.js
+++ frontend/src/campaign/campaignView.js
@@ -30,6 +30,95 @@ const get_default_proposal = (topic_names) => {
     return proposal;
 };
 
+function getDragAfterElement(props, y) {
+    const draggableElements = [...props.querySelectorAll('.draggable:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return {
+                offset: offset,
+                element: child
+            };
+        }
+        return closest;
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
+function Flexboard(props) {
+    const drop = (e) => {
+        e.preventDefault();
+        const cardId = e.dataTransfer.getData('cardId');
+
+        const card = document.getElementById(cardId);
+        card.style.display = 'block';
+
+        e.target.appendChild(card);
+    };
+
+    const dragOver = (e) => {
+        e.preventDefault();
+        // const afterElement = getDragAfterElement(props, e.ClientY);
+        // console.log(afterElement);
+    };
+
+    return (
+        <div
+            id={props.id}
+            className={props.className}
+            onDrop={drop}
+            onDragOver={dragOver}
+        >
+            { props.children }
+        </div>
+    );
+}
+
+
+function Flexcard(props) {
+    const dragStart = (e) => {
+        const target = e.target;
+
+        e.dataTransfer.setData('cardId', target.id);
+
+        // setTimeout(() => {
+        //    target.style.display = 'none';
+        // }, 0);
+    };
+
+    const dragOver = (e) => {
+        e.stopPropagation();
+    };
+
+    return (
+        <div
+            id={props.id}
+            className={props.children}
+            draggable={props.draggable}
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+        >
+            { props.children }
+        </div>
+    );
+}
+
+
+Flexboard.propTypes = {
+    id: PropTypes.string,
+    className: PropTypes.string,
+    children: PropTypes.func
+};
+
+
+Flexcard.propTypes = {
+    id: PropTypes.string,
+    className: PropTypes.string,
+    children: PropTypes.func,
+    draggable: PropTypes.bool
+};
+
 // const all_topics = [
 //     'Health services',
 //     'Education',
@@ -253,6 +342,24 @@ class Speech extends React.Component {
                         <span>High priority</span>
                     </div>
                     {topics}
+                </div>
+                <div className='flexbox'>
+                    <Flexboard id='board-1' className='flexboard'>
+                        <Flexcard id='card1' className='flexcard' draggable='true'>
+                            <p
+                                className='flexcard'
+                            >
+                                Card one
+                            </p>
+                        </Flexcard>
+                        <Flexcard id='card2' className='flexcard' draggable='true'>
+                            <p
+                                className='flexcard'
+                            >
+                                Card two
+                            </p>
+                        </Flexcard>
+                    </Flexboard>
                 </div>
                 <div className='reset_button'>
                     <button
