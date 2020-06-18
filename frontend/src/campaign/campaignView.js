52514be5d01efeb48eb45434615f7616754f62c4
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2020-06-18 15:27:57 -0400

    Revert "Merge branch 'campaign-clarifying_priority_points' into campaign-updating_story"
    This reverts commit 809ca7e625c00638cc82bbf88e5e6fe79aa7c0c2, reversing
    changes made to a2dcf311a7e96d3a4ea1a95cda8fab6bb050225b.

--- frontend/src/campaign/campaignView.js
+++ frontend/src/campaign/campaignView.js
@@ -265,9 +265,6 @@ export class CampaignView extends React.Component {
                 }
                 sentence += trait;
             });
-            if (issueList.length === 0) {
-                sentence = ' ';
-            }
             desc.push(<div key={i}>{sentence}</div>);
             desc.push(<br key={i}/>);
         });
