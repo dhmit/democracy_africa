37ed816d20e7239655485551c99d773dab214cf7
Jason Lin <jasonlin43212@gmail.com>
2020-06-26 14:55:17 -0400

    added template for adding in staff members

--- frontend/src/about/about.js
+++ frontend/src/about/about.js
@@ -1,9 +1,51 @@
 import React from 'react';
 
+const staffMembers = [
+    {
+        name: 'Michael Scott Cuthbert',
+        title: 'Faculty Director & Associate Professor',
+        photoSrc: '',
+    },
+    {
+        name: 'Ryaan Ahmed',
+        title: 'Technical Director Senior Research Engineer',
+        photoSrc: '',
+    },
+    {
+        name: 'Erica Zimmer',
+        title: 'Postdoctoral Associate',
+        photoSrc: '',
+    },
+    {
+        name: 'Nicole Fountain',
+        title: 'Administrative Assistant',
+        photoSrc: '',
+    },
+];
+
+const studentMembers = [
+    {
+        name: 'Michelle He',
+        photoSrc: ''
+    },
+    {
+        name: 'Justice Vidal',
+        photoSrc: ''
+    },
+    {
+        name: 'Jason Lin',
+        photoSrc: ''
+    },
+    {
+        name: 'Ning-Er Lei',
+        photoSrc: ''
+    },
+];
+
 class About extends React.Component {
     render() {
         return (
-            <div>
+            <div className='about'>
                 <div className='about-title'>
                     About This Project
                 </div>
@@ -19,13 +61,21 @@ class About extends React.Component {
                         the EdX Course Democracy and Development: Perspectives from Africa.
                     </p>
                 </div>
-                <div>
+                <div className='team'>
                     <div className='about-title'>Staff and Members</div>
                     <div className='staff'>
-
+                        {staffMembers.map((member, k) => (
+                            <div className='staff-member' key={k}>
+                                {member.name}
+                            </div>
+                        ))}
                     </div>
-                    <div className='members'>
-
+                    <div className='students'>
+                        {studentMembers.map((member, k) => (
+                            <div className='student-member' key={k}>
+                                {member.name}
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
