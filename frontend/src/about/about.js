ab99a4168bf1b4977c52b769329a82d827cb8cde
Jason Lin <jasonlin43212@gmail.com>
2020-06-26 14:45:15 -0400

    started to structure the about page

--- frontend/src/about/about.js
+++ frontend/src/about/about.js
@@ -7,6 +7,27 @@ class About extends React.Component {
                 <div className='about-title'>
                     About This Project
                 </div>
+                <div className='about-text'>
+                    <p>
+                        <i>Gamifying "Democracy and Development: Perspectives from Africa"</i> is a
+                        project by the MIT Programs in Digital Humanities in collaboration with our
+                        Spring 2020 Faculty Fellow, Evan Lieberman, Total Professor of Political
+                        Science and Contemporary Africa at MIT.
+                    </p>
+                    <p>
+                        We are collaborating to create interactive simulations to integrate into
+                        the EdX Course Democracy and Development: Perspectives from Africa.
+                    </p>
+                </div>
+                <div>
+                    <div className='about-title'>Staff and Members</div>
+                    <div className='staff'>
+
+                    </div>
+                    <div className='members'>
+
+                    </div>
+                </div>
             </div>
         );
     }
